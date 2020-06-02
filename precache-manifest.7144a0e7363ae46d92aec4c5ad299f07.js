ac6655b156231f8e9da112bc24931d08b4b2a80e
Studio Fomalhaut <studio@fomalhaut.nl>
2020-06-02 13:00:33 +0100

    Add files via upload

---
+++ precache-manifest.7144a0e7363ae46d92aec4c5ad299f07.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab630e1b307219de11c9b6d3e25d8df0",
+    "url": "/WeTube/index.html"
+  },
+  {
+    "revision": "a093dadb732feee00d10",
+    "url": "/WeTube/static/css/main.dd0d9dac.chunk.css"
+  },
+  {
+    "revision": "dabef5c1d032557708fe",
+    "url": "/WeTube/static/js/2.3f9b14f1.chunk.js"
+  },
+  {
+    "revision": "13b685b3db9ae92c2d07c213e5e3506f",
+    "url": "/WeTube/static/js/2.3f9b14f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a093dadb732feee00d10",
+    "url": "/WeTube/static/js/main.f7789017.chunk.js"
+  },
+  {
+    "revision": "f9d8763ab154640a857d",
+    "url": "/WeTube/static/js/runtime-main.55f33f7d.js"
+  }
+]);
